ba3883ca58659a3bfbcad00d8dac5d77dd559454
NAIRB <sohhanyu@gmail.com>
2025-10-07 03:11:04 +0800

    Dark Outdoor Videos Added

--- examples/public/assets/AR_Ruler/rgbd_ar_ruler_main.js
+++ examples/public/assets/AR_Ruler/rgbd_ar_ruler_main.js
@@ -115,6 +115,26 @@ const VIDEO_CONFIG = {
     leftFile: 'Indoor_Videos/Balcony_left_dark_35cm.mp4',
     rightFile: 'Indoor_Videos/Balcony_right_dark_35cm.mp4',
     displayName: '8. dark_balcony_LF_35cm'
+  },
+  '9. dark_alley_LF_150cm': {
+    leftFile: 'Outdoor_Videos/Alley_left_dark_150cm.mp4',
+    rightFile: 'Outdoor_Videos/Alley_right_dark_150cm.mp4',
+    displayName: '9. dark_alley_LF_150cm'
+  },
+  '10. dark_carpark_LF_66cm': {
+    leftFile: 'Outdoor_Videos/Carpark_left_dark_66cm.mp4',
+    rightFile: 'Outdoor_Videos/Carpark_right_dark_66cm.mp4',
+    displayName: '10. dark_carpark_LF_66cm'
+  },
+  '11. dark_walkway_LF_150cm': {
+    leftFile: 'Outdoor_Videos/Walkway_left_dark_150cm.mp4',
+    rightFile: 'Outdoor_Videos/Walkway_right_dark_150cm.mp4',
+    displayName: '11. dark_walkway_LF_150cm'
+  },
+  '12. dark_pillar_LF_150cm': {
+    leftFile: 'Outdoor_Videos/Pillar_left_dark_150cm.mp4',
+    rightFile: 'Outdoor_Videos/Pillar_right_dark_150cm.mp4',
+    displayName: '12. dark_pillar_LF_150cm'
   }
 };
 
